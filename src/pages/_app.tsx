4701f8d9b789b1da04f9086b68e38631f85fc704
Supanut1911 <ynotmela@gmail.com>
2023-04-16 17:40:12 +0700

    change set up queryClient to global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
@@ -10,9 +12,14 @@ import '@/styles/colors.css';
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <div>
-      <Component {...pageProps} />;
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />;
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,18 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 import Layout from '@/components/layout/Layout';
 import PostView from '@/components/posts/posts';
 
 const Post = () => {
-  const queryClient = new QueryClient();
-
   return (
     <Layout>
-      <QueryClientProvider client={queryClient}>
-        <PostView />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+      <PostView />
     </Layout>
   );
 };
